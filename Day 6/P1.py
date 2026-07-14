340e0c0e61c13c1535e71883686d76b7db590049
Saish Panhalkar <panhalkarsaish@gmail.com>
2026-07-14 19:37:00 +0530

    Add Day 6 folder

---
+++ Day 6/P1.py
@@ -0,0 +1,13 @@
+#Loops
+fruits=['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+
+#for loop with range
+for i in range(5):
+    print (i)
+
+#for loop with dictionary
+person = {'name':'Saish','age':20,'city':'Mumbai'}
+for key,value in person.items():
+    print(f"{key}:{value}")
